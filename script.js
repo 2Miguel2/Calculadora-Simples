5c618ab398b105f06586fe1581f1de671a432978
2Miguel2 <schoolmiguel8@gmail.com>
2025-05-18 22:29:51 -0300

    update
    troquei cor de fundo para roxo e coloquei mais funcionalidades.

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const placeholder = document.getElementById('placeholder');
 
 function atualizarPlaceholder() {
   placeholder.style.display = tela.value === '' ? 'block' : 'none';
-}
+} //Faz aparecer e desparecer o "Esperando uma conta..." o block faz aparecer e o none sumir quando digitado algo
 
 function adicionar(valor) {
+    if (tela.value == 'Error') {
+        tela.value = '';
+    }
+
     tela.value += valor;
     atualizarPlaceholder();
 }
@@ -22,11 +26,16 @@ function deletar() {
 
 function calcular() {
     try {
-        tela.value = eval(tela.value);
+        let expressao = tela.value
+            .replace(/x/g, '*')                 // x → *
+            .replace(/(\d+)%/g, '($1/100)');    // 50% → (50/100)
+
+        tela.value = eval(expressao);
     } catch (e) {
-        tela.value = 'Erro';
+        tela.value = 'Error';
     }
     atualizarPlaceholder();
 }
+
 
 atualizarPlaceholder();
